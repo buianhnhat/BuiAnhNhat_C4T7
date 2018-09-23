e56f4c59ad459d4d7370193e1d7fe00323dd3cc9
bui anh nhat <buianhnhat2003@gmail.com>
2018-09-23 18:12:42 +0700

    jjj

--- Mini_Hack/test.py
+++ Mini_Hack/test.py
@@ -1,2 +1,39 @@
-n = "123"
-print (n.isdigit())
+from random import randint
+
+while True:
+    x = randint (0, 20)
+    y = randint (0, 20)
+    z = randint (21, 22)
+    p = randint (-1, 1)
+    k = x+y+p
+    q = x-y+p
+    if (z==21):
+        print (x, "+", y, "=", k)
+        kq = input ("(Y/N)? ")
+        if (x+y==k):
+            if (kq == "Y"):
+                print ("True True True")
+            else:
+                print ("False - GAME OVER")
+                break
+        else:
+            if (kq == "N"):
+                print ("True True True")
+            else:
+                print ("False - GAME OVER!!!")
+                break
+    else:
+        print (x, "-", y, "=", q)
+        kq = input ("(Y/N)? ")
+        if (x-y==q):
+            if (kq == "Y"):
+                print ("True True True")
+            else:
+                print ("False - GAME OVER")
+                break
+        else:
+            if (kq == "N"):
+                print ("True True True")
+            else:
+                print ("False - GAME OVER")
+                break
